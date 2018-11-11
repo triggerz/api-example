b16c20c87f0d7b489857af65e9456572f574ea18
Lars Thorup <lars@triggerz.com>
2018-11-11 12:01:50 +0100

    POST member/byEmail/:email

--- index.js
+++ index.js
@@ -28,6 +28,20 @@ async function apiGet (host, endpoint, accessToken) {
   return response;
 }
 
+async function apiPost (host, endpoint, body, accessToken) {
+  const options = {
+    url: `${host}/api/${endpoint}`,
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    },
+    body,
+    json: true
+  };
+  const response = await request.post(options);
+  return response;
+}
+
 async function main () {
   if (!process.env.TRIGGERZ_CLIENT_ID || !process.env.TRIGGERZ_CLIENT_SECRET) {
     console.error('ERROR: Both environment variables TRIGGERZ_CLIENT_ID and TRIGGERZ_CLIENT_SECRET must be set before running this demo.');
@@ -43,6 +57,26 @@ async function main () {
 
   console.log('Response from triggerz API:', pingResponse);
 
+  console.log('Requesting "GET memberRelationType/list"');
+  const memberRelationTypeListStringResponse = await apiGet(host, 'memberRelationType/list', accessToken);
+  const memberRelationTypeListResponse = JSON.parse(memberRelationTypeListStringResponse);
+  console.log(JSON.stringify(memberRelationTypeListResponse, null, 2));
+
+  console.log('Requesting "GET segmentGroup/list"');
+  const segmentGroupListStringResponse = await apiGet(host, 'segmentGroup/list', accessToken);
+  const segmentGroupListResponse = JSON.parse(segmentGroupListStringResponse);
+  console.log(JSON.stringify(segmentGroupListResponse, null, 2));
+
+  console.log('Requesting "GET segment/list"');
+  const segmentListStringResponse = await apiGet(host, 'segment/list', accessToken);
+  const segmentListResponse = JSON.parse(segmentListStringResponse);
+  console.log(JSON.stringify(segmentListResponse, null, 2));
+
+  console.log('Requesting "GET terms/list"');
+  const termsListStringResponse = await apiGet(host, 'terms/list', accessToken);
+  const termsListResponse = JSON.parse(termsListStringResponse);
+  console.log(JSON.stringify(termsListResponse, null, 2));
+
   console.log('Requesting "GET member/list"');
   const memberListStringResponse = await apiGet(host, 'member/list', accessToken);
   const memberListResponse = JSON.parse(memberListStringResponse);
@@ -55,6 +89,22 @@ async function main () {
     const memberStringResponse = await apiGet(host, `member/byEmail/${email}`, accessToken);
     const memberResponse = JSON.parse(memberStringResponse);
     console.log(JSON.stringify(memberResponse, null, 2));
+
+    console.log('Request "POST member/byEmail/:email"');
+    const memberPatch = {
+      entity: {
+        account: {
+          displayName: 'Ronja Totally'
+        },
+        memberSegment: [
+          {import: 'delete', segmentRef: 'engineering'},
+          {import: 'insert', segmentRef: 'sales'}
+        ]
+      }
+    };
+    console.log(JSON.stringify(memberPatch, null, 2));
+    const memberRef = await apiPost(host, `member/byEmail/${email}`, memberPatch, accessToken);
+    console.log(memberRef);
   }
 }
 
--- readme.md
+++ readme.md
@@ -4,16 +4,24 @@ This example demonstrates in the most basic way how to communicate with the Trig
 
 It will start by requesting an access token given the client id and secret that you will have received from Triggerz. Given this, it makes a request to the `ping` endpoint and log the response.
 
-The code is found in the [index.js](index.js) file. It runs on [nodejs](https://nodejs.org/). Before you can run the sample, you need to set environment variables `TRIGGERZ_CLIENT_ID` and `TRIGGERZ_CLIENT_SECRET` to the values that we have given you.
+The code is found in the [index.js](index.js) file. It runs on [nodejs](https://nodejs.org/). Before you can run the sample, you need to set environment variables `TRIGGERZ_CLIENT_ID` and `TRIGGERZ_CLIENT_SECRET` to the values you have received from Triggerz.
 
-Then you should be able to run it by typing `npm start`. If everything is set up correctly, you should see something like this:
+Then you should be able to run it with `npm start`.
 ```
 $ npm install
 $ set TRIGGERZ_CLIENT_ID=...
 $ set TRIGGERZ_CLIENT_SECRET=...
 $ npm start
+```
 
+If everything is set up correctly, you should see something like this:
+
+```
 Requesting access token..
 Pinging Triggerz API..
 Response from triggerz API: "pong"
 ```
+
+Full sample output from a test organization is included in [sample-output.log](sample-output.log).
+
+You can edit index.js and specify the email of a user in your organization instead of `ronja.demo@triggerz.com` to verify some of the other end-points.
